f53888fe37e30a9c64a6903367c4e8cde74604cd
Benjamin Renard <benjamin.renard@akka.eu>
2025-01-20 07:54:47 +0100

    WIP

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,7 @@ BASEDIR=$(dirname "$0")
 PORT=${1:-6543}
 NAME=${2:-speasy_proxy}
 SPEASY=${3:-speasy}
-docker build --build-arg PORT=$PORT --build-arg SPEASY=$SPEASY -t $NAME -f $BASEDIR/Dockerfile $BASEDIR/../
+
+read -p "IRAP Gitlab user: " GITLAB_USER
+read -p "IRAP Gitlab password: " GITLAB_PWD
+docker build --no-cache --build-arg PORT=$PORT --build-arg SPEASY=$SPEASY --build-arg GITLAB_USER=$GITLAB_USER --build-arg GITLAB_PWD=$GITLAB_PWD -t $NAME -f $BASEDIR/Dockerfile $BASEDIR/../
